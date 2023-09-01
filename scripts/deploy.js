73b6d17bccb44d0ac78a29794f83a4e240a8f8fa
ShivaniJaggi16 <137707056+ShivaniJaggi16@users.noreply.github.com>
2023-09-01 19:45:54 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,36 +8,19 @@ const hre = require("hardhat");
 
 async function main() {
   const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("ETH");
+  const Assessment = await hre.ethers.getContractFactory("Hardhat");
   const assessment = await Assessment.deploy(initBalance);
   await assessment.deployed();
 
-  assessment.DisplayAddress();
-
   console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
-
-  assessment.on("showAddress", (owner) => {
-    console.log(`msg.sender is : ${owner}`);
-  })
-
-  assessment.on("deposite", (deposit_val, Balance) => {
-    console.log(`New deposit: ${deposit_val} new Balance is : ${Balance} ETH`);
-  })
-
-  assessment.on("withdraw", (withdraw_val, Balance) => {
-    console.log(`New withdraw: ${withdraw_val} new Balance is : ${Balance} ETH`);
-  })
-
-  // Redeem function
-  assessment.on("redeem", (amount) => {
-    console.log(`Redeemed amount: ${amount} ETH`);
-  });
-
-  // Call the "redeem" function
-  await assessment.Redeem();
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+ 
